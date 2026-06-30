e56c2ce1e02773063b4e7a35a30d186d1cf9b588
Bruno Ferreira <pittanaferreira@gmail.com>
2026-06-30 18:58:05 -0300

    Updates!

--- src/main/java/com/designaciones/webdesignaciones/config/CorsConfig.java
+++ src/main/java/com/designaciones/webdesignaciones/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Aplica a todos los endpoints (incluyendo /api/arbitros y /api/canchas)
                 .allowedOrigins(
-                        "http://localhost:5133",
+                        "http://localhost:5174",
                         "http://localhost:5173",
                         "http://localhost:5500",
                         "http://localhost:5501",
